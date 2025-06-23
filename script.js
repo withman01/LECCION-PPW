8b3fdcc4380db35960db5fb9ea41b0c454821e23
Withman01 <renegarrido02@gmail.com>
2025-06-22 19:39:20 -0500

    agregamos movimiento y rebote en las paredes

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function moverPaletaJugador(evt) {
 
   player.y = mouseY - player.height / 2;
 }
+
+function gameLoop() {
+  update();
+  render();
+}
+
+function update() {
+  ball.x += ball.velocityX;
+  ball.y += ball.velocityY;
+
+  if (ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height) {
+    ball.velocityY = -ball.velocityY;
+  }
+}
